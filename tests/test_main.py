221e9af20b3ec63702f7ecd5944282dba19ffb3a
Cooper <cooper.weston@outlook.com>
2025-08-01 20:58:17 +1000

    Added testing for Enemy initialization passed.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,13 @@
 import pytest
-from tests.main import Character
+from main import Character, Enemy
 
 def test_character_initialization():
     c = Character()
     assert hasattr(c, 'inventory')
+
+def test_enemy_initialization():
+    e = Enemy("Goblin", 30, "desc", 4)
+    assert e.name == "Goblin"
+    assert e.life == 30
+    assert e.attack == 4
+
